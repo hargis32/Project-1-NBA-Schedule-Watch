2dae603b3628608bda294e29b6bca276792e0411
Talhah Awan <talhahawan@gmail.com>
2021-09-18 01:06:57 -0500

    added js code to show list of games and game time, still need to clean up but it is working! Added a container in HTML to show data from JS.

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 2cd0953619ac9ea73ff662e74b9b14fe4f60cd39
+const nbaGames = async function() {
+    var gamesEl = document.getElementById('games')
+    var games = await fetch('https://gy62hh4w41.execute-api.us-east-1.amazonaws.com/default/nbagames', {});
+    var gamesArray = await games.json();
+    await gamesArray.forEach(game => {
+        gamesEl.innerHTML += game.homeTeam.teamName;
+        gamesEl.innerHTML += ' vs '
+        gamesEl.innerHTML += game.awayTeam.teamName;
+        gamesEl.innerHTML += ' '
+        gamesEl.innerHTML += game.gameDateTimeEst;
+        gamesEl.innerHTML += '<br>'
+    });
+}
