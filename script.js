a8ab9ea575d2e7b0346bce2a89e8f99055746e11
Talhah Awan <talhahawan@gmail.com>
2021-09-20 20:30:38 -0500

    Js updated to grab specific dates when ran

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const nbaGames = async function() {
     var gamesEl = document.getElementById('games')
-    var games = await fetch('https://gy62hh4w41.execute-api.us-east-1.amazonaws.com/default/nbagames', {});
+    var games = await fetch('https://gy62hh4w41.execute-api.us-east-1.amazonaws.com/default/nbagames');
     var gamesArray = await games.json();
-    await gamesArray.forEach(game => {
-        gamesEl.innerHTML += game.homeTeam.teamName;
-        gamesEl.innerHTML += ' vs '
-        gamesEl.innerHTML += game.awayTeam.teamName;
-        gamesEl.innerHTML += ' '
-        gamesEl.innerHTML += game.gameDateTimeEst;
-        gamesEl.innerHTML += '<br>'
+
+    // example date
+    var today = new Date("10/03/2021");
+    var gamesForDate = getGamesInDate(today, gamesArray);
+    console.log("TODAY'S GAMES:", gamesForDate);
+
+//     await gamesArray.forEach(game => {
+//         gamesEl.innerHTML += game.homeTeam.teamName;
+//         gamesEl.innerHTML += ' vs '
+//         gamesEl.innerHTML += game.awayTeam.teamName;
+//         gamesEl.innerHTML += ' '
+//         gamesEl.innerHTML += game.gameDateTimeEst;
+//         gamesEl.innerHTML += '<br>'
+//     });
+}
+
+function getGamesInDate(date, games) {
+    return games.filter(game => {
+        var gameDate = new Date(game.gameDateTimeUTC);
+        return date.getFullYear() === gameDate.getFullYear() && date.getMonth() === gameDate.getMonth() && date.getDate() === gameDate.getDate();
     });
 }
