0e337e74c5efadf752ae15d5b02cbf7d732bbe51
Talhah Awan <talhahawan@gmail.com>
2021-09-23 17:46:19 -0500

    Js now works as its supposed to. You can insert the date into the input field and it returns the corresponding NBA game on that date

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-const nbaGames = async function() {
-    var gamesEl = document.getElementById('games')
+const inputDate = document.querySelector("#search-games")
+
+const nbaGames = async function(event) {
+    event.preventDefault();
+    var gamesEl = document.getElementById('display-games');
     var games = await fetch('https://gy62hh4w41.execute-api.us-east-1.amazonaws.com/default/nbagames');
     var gamesArray = await games.json();
-
+    console.log(userDate);
     // example date
-    var today = new Date("10/03/2021");
-    var gamesForDate = getGamesInDate(today, gamesArray);
+    var userDate = new Date(inputDate.value);
+    var gamesForDate = getGamesInDate(userDate, gamesArray);
     console.log("TODAY'S GAMES:", gamesForDate);
 
-//     await gamesArray.forEach(game => {
-//         gamesEl.innerHTML += game.homeTeam.teamName;
-//         gamesEl.innerHTML += ' vs '
-//         gamesEl.innerHTML += game.awayTeam.teamName;
-//         gamesEl.innerHTML += ' '
-//         gamesEl.innerHTML += game.gameDateTimeEst;
-//         gamesEl.innerHTML += '<br>'
-//     });
+    await gamesForDate.forEach(game => {
+        gamesEl.innerHTML += game.homeTeam.teamName;
+        gamesEl.innerHTML += ' vs '
+        gamesEl.innerHTML += game.awayTeam.teamName;
+        gamesEl.innerHTML += ' '
+        gamesEl.innerHTML += game.gameDateTimeEst;
+        gamesEl.innerHTML += '<br>'
+    });
 }
 
 function getGamesInDate(date, games) {
@@ -24,3 +27,5 @@ function getGamesInDate(date, games) {
         return date.getFullYear() === gameDate.getFullYear() && date.getMonth() === gameDate.getMonth() && date.getDate() === gameDate.getDate();
     });
 }
+
+document.getElementById("games").addEventListener("submit", nbaGames)
