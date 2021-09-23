4a4ade88451869af1d710626c12860b63d22d850
Jon Lynch <jonnylynchy@gmail.com>
2021-09-22 20:25:55 -0500

    added game search functionality, needs to finish up by adding games to dom; also fixed some general javascript issues and syntax problems

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 var key = "OdxfzW97A9GgejJGoakDEF76rmEVTHu2"
 
 function handleLocationFormSubmit(event) {
@@ -15,15 +13,15 @@ function handleLocationFormSubmit(event) {
     else if (!locationInputVal) {
         console.error('Not a valid address. Please try again.');
     }
-    else (!radiusInputVal) {
+    else if (!radiusInputVal) {
         console.error('Not a valid radius. Please enter number');
     }
-        return;
-    };
+    return;
+};
 
 var getLocalBar = function (locationInputVal, radiusInputVal) {
     // need to double check endpoints and paramaters. 581301 refers to code for "Bars" as default restaurant type 
-    var apiURL = "http://www.mapquestapi.com/search/v2/radius?origin=" + locationInputVal +"&radius=" + radiusInputVal + "maxMatches=6&ambiguities=ignore&hostedData=mqap.ntpois|group_sic_code=?|581301&outFormat=json&key=" + key;
+    var apiURL = "http://www.mapquestapi.com/search/v2/radius?origin=" + locationInputVal + "&radius=" + radiusInputVal + "maxMatches=6&ambiguities=ignore&hostedData=mqap.ntpois|group_sic_code=?|581301&outFormat=json&key=" + key;
 
     fetch(apiURL)
         .then(function (response) {
@@ -55,9 +53,50 @@ var displayBars = function () {
     mapEl.appendChild(name);
     mapEl.appendChild(location);
     mapEl.appendChild(phoneNum);
-
-    
 };
+// YOU NEED TO CREATE A SELECTOR FOR THIS ELEMENT.
+// searchformEl.addEventListener("submit", handleLocationFormSubmit);
 
-searchformEl.addEventListener("submit", handleLocationFormSubmit);
 
+
+/*
+    GAMES CODE ===============================
+*/
+
+// global variable holds all games
+var gamesArray;
+
+// Get all the games and put in array
+async function getGames() {
+    var games = await fetch('https://gy62hh4w41.execute-api.us-east-1.amazonaws.com/default/nbagames');
+    gamesArray = await games.json();
+}
+
+var gamesFormEl = document.querySelector("#form-games");
+gamesFormEl.addEventListener("submit", searchGames);
+
+function searchGames(event) {
+    event.preventDefault();
+
+    // get date from form text field
+    var dateValue = document.querySelector("#search-games").value;
+    // create date from text field
+    var userDate = new Date(dateValue);
+    // call function to filter games
+    var gamesForDate = getGamesInDate(userDate, gamesArray);
+    // do something with data (create some dom nodes)
+    console.log("TODAY'S GAMES:", gamesForDate);
+
+    // HERE'S WHERE YOU WOULD TAKE gamesForDate AND OUTPUT/RENDER TO DOM
+}
+
+// filters games array by date
+function getGamesInDate(date, games) {
+    return games.filter(game => {
+        var gameDate = new Date(game.gameDateTimeUTC);
+        return date.getFullYear() === gameDate.getFullYear() && date.getMonth() === gameDate.getMonth() && date.getDate() === gameDate.getDate();
+    });
+}
+
+// initialize get games to fetch all games
+getGames();
